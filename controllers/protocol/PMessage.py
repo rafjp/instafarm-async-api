9dbc0e96a31d06212574786d5027b6247f69aaf2
rafael job <rrafaelljob@gmail.com>
2022-06-17 21:09:41 -0300

    fix message view

--- controllers/protocol/PMessage.py
+++ controllers/protocol/PMessage.py
@@ -9,8 +9,10 @@ async def send_message(user_from: MBUser, user_to: MBUser, message: str) -> dict
     """
 
     message: MBMessage = MBMessage.register_message(
-        message=message, from_channel=user_from.message_channel,
-        user_id_own=user_to.id, from_user_name=user_from.name
+        message=message,
+        from_channel=user_from.message_channel,
+        user_id_own=user_to.id,
+        from_user_name=user_from.name,
     )
     await message.commit()
 
@@ -18,7 +20,7 @@ async def send_message(user_from: MBUser, user_to: MBUser, message: str) -> dict
         "type": "message",
         "call": "send_message",
         "message_id": str(message.id),
-        "at": message.create_at.isoformat()
+        "at": message.create_at.isoformat(),
     }
 
 
@@ -27,23 +29,25 @@ async def get_massages(user: MBUser):
     Return the user messages.
     """
 
-    message_cursor: Cursor = MBMessage.find({'user_id_own': user.id})
+    message_cursor: Cursor = MBMessage.find({"user_id_own": user.id})
     messages = []
 
     async for message in message_cursor:
-        messages.append({
-            "at": message.create_at.isoformat(),
-            "contact_message_channel": message.from_channel,
-            "contact_name": message.from_user_name,
-            "message_id": str(message.id),
-            "message": message.message
-        })
+        messages.append(
+            {
+                "at": message.create_at.isoformat(),
+                "contact_message_channel": message.from_channel,
+                "contact_name": message.from_user_name,
+                "message_id": str(message.id),
+                "message": message.message,
+            }
+        )
 
     return {
         "type": "message",
         "call": "get_massages",
         "user_id": str(user.id),
-        "messages": messages
+        "messages": messages,
     }
 
 
@@ -52,15 +56,11 @@ async def clean_messages(user: MBUser):
     Erases all user messages.
     """
 
-    message_cursor: Cursor = MBMessage.find({'user_id_own': user.id})
+    message_cursor: Cursor = MBMessage.find({"user_id_own": user.id})
 
     count = 0
     async for message in message_cursor:
         await message.delete()
         count += 1
 
-    return {
-        "type": "message",
-        "call": "clean_messages",
-        "message_count": count
-    }
+    return {"type": "message", "call": "clean_messages", "message_count": count}
--- views/MBMessageView.py
+++ views/MBMessageView.py
@@ -1,12 +1,11 @@
+from controllers.MBAuthScope import MBAuthScope
+from controllers.MBRequest import MBRequest
+from controllers.protocol import PMessage
+from models.MBUser import MBUser
 from sanic import Blueprint, json
 from sanic.response import Request
 from sanic_jwt import inject_user, scoped
 from sanic_openapi import doc
-from controllers.MBAuthScope import MBAuthScope
-
-from controllers.protocol import PMessage
-from models.MBUser import MBUser
-from controllers.MBRequest import MBRequest
 
 message_api = Blueprint("Message", url_prefix="/message/")
 
@@ -19,7 +18,7 @@ message_api = Blueprint("Message", url_prefix="/message/")
 @message_api.post("send")
 @inject_user()
 @scoped(MBAuthScope.USER, require_all=False)
-async def send_message(request: Request, user_from: MBUser):
+async def send_message(request: Request, user: MBUser):
     if "to_message_channel" not in request.args:
         return MBRequest.response_invalid_params(["to_message_channel"])
 
@@ -30,19 +29,19 @@ async def send_message(request: Request, user_from: MBUser):
     if user_to is None:
         return MBRequest.invalid_user_channel_id(request.args.get("to_message_channel"))
 
-    return json(await PMessage.send_message(user_from=user_from, user_to=user_to, message=request.args.get("message")))
+    return json(await PMessage.send_message(user_from=user, user_to=user_to, message=request.args.get("message")))
 
 
 @doc.summary("Get user messages")
-@message_api.get("/<user_id>")
+@message_api.get("/")
 @inject_user()
 @scoped(MBAuthScope.USER, require_all=False)
 async def get_massages(request: Request, user: MBUser):
-    return json(await PMessage.get_massages(user=user))
+    return json(await PMessage.get_massages(user=user)) 
 
 
 @doc.summary("Delete all messages for the user")
-@message_api.delete("clean/<user_id>")
+@message_api.delete("clean")
 @inject_user()
 @scoped(MBAuthScope.USER, require_all=False)
 async def remove_message(request: Request, user: MBUser):
